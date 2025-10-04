84eaddd0e8153c067ac128fc5a4975d61c90d679
Kjlee122 <147563359+kjlee122@users.noreply.github.com>
2025-10-04 06:49:10 +0000

    fin

--- DiceRoll.py
+++ DiceRoll.py
@@ -8,11 +8,19 @@ def main():
   #Create an empty list with possible roll values
   rolls = [0,0,0,0,0,0,0,0,0,0,0,0]
   #Create two dice values ranging from 1 - 6 each
-  
+  for r in range(10000):
+    dice1 = random.randint(1,6)
+    dice2 = random.randint(1,6)
+    rolls[(dice1 + dice2) - 1] = rolls[(dice1 + dice2) -1] + 1
   #find the sum total of the two dice
   
   #print statictics for dice rolls
-
+  dice = 1
+  for count in rolls:
+    percent = ((count / 10000) * 100)
+    print(dice,":", count, "Percent: ", percent)
+    dice = dice + 1
+  
 
 if __name__ == '__main__':
   main()
--- WordCount.py
+++ WordCount.py
@@ -5,9 +5,25 @@
 
 def main():
   textFile = open("gettysberg.txt", 'r')
+  lineCount = 0
+  wordCount = 0 
+  letterCount = 0 
   
   for line in textFile:
-    print(line)
+    lineCount = lineCount + 1
+    words = line.split()
+    print(words)
+    for w in words:
+      wordCount = wordCount + 1
+    letters = list(line)
+    for char in letters:
+      if char.isalpha():
+        letterCount = letterCount + 1
+  
+
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
+  print("Letters:", letterCount)
   
 
 if __name__ == '__main__':
